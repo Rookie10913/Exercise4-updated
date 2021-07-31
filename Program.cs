4033eae3ab25d274bd3d2cc25f13ce83a9f339f6
Rookie10913 <64032388+Rookie10913@users.noreply.github.com>
2021-07-31 14:18:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Excercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cirle c = new Cirle(2);
+            Console.WriteLine(c.Area());
+            Rectangle r = new Rectangle(2, 3);
+            Console.WriteLine(r.Area());
+        }
+    }
+    public abstract class Shape
+    {
+        public abstract double Area();
+    }
+    public class Cirle : Shape
+    {
+        private double r;
+        private double PI = Math.PI;
+        public Cirle(double r)
+        {
+            this.r = r;
+        }
+        public override double Area()
+        {
+            Console.WriteLine("Circular area is:");
+            return PI * r * r;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        private double w, h;
+        public Rectangle(double w, double h)
+        {
+            this.w = w;
+            this.h = h;
+        }
+        public override double Area()
+        {
+            Console.WriteLine("The area of ​​the rectangle is:");
+            return w * h;
+        }
+    }
+
+}
